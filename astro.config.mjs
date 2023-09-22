e51e99138ab924402b0ca73a3a274fed165329d7
Gil Nimer <gil.family.nimer@gmail.com>
2023-09-22 18:37:19 +0300

    pwa support
    pwa support

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,9 @@
 import { defineConfig } from "astro/config";
+import AstroPWA from "@vite-pwa/astro";
 
 // https://astro.build/config
 export default defineConfig({
+  integrations: [AstroPWA({})],
   image: {
     remotePatterns: [{ protocol: "https" }],
     hostname: "ik.imagekit.io",
