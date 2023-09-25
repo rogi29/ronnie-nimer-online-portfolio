1b96f18af705e77c33acc66a3e73d4ad58c2a323
Gil Nimer <gilmarianimer@gmail.com>
2023-09-25 15:10:19 +0300

    added structured data
    added structured data

---
+++ src/data/deliverable.ts
@@ -0,0 +1,31 @@
+export default [
+  {
+    name: "Under Strange Suns Final Design",
+    description:
+      "A weary hero roams a bizarre world.\nHot stars hang heavy over a scorched landscape where darkness never arrives.",
+    image: "https://ik.imagekit.io/8hod93w55/1.jpg",
+  },
+  {
+    name: "The Ambrosian Kingdom Character Design",
+    description: "The famous knight and his two mercenaries.",
+    image: "https://ik.imagekit.io/8hod93w55/4.jpg",
+  },
+  {
+    name: "The Ambrosian Kingdom Armor Design",
+    description:
+      "The Ambrosian People find the curvature of the armor aesthetically pleasing. Therefore, it is usually accentuated or exaggerated on Ambrosian armors.",
+    image: "https://ik.imagekit.io/8hod93w55/6.jpg",
+  },
+  {
+    name: "The Two-Headed Gnome Stylized Version",
+    description:
+      "Meet Humphrey and Trummfy, the two-headed Gnome. Humphrey is the one who is constantly in a good mood, kind and optimistic. Trummfy is the grumpy one, who enjoys reading despite being nearly blind, which makes him even more moody.\nPumpkin is their favorite delicacy. They enjoy traveling and solving difficult puzzles, making them excellent companions on adventures.",
+    image: "https://ik.imagekit.io/8hod93w55/9.jpg",
+  },
+  {
+    name: "The Two-Headed Gnome Final Design",
+    description:
+      "Humphrey and Trummfy, the two-headed Gnome. Game Production Final Design",
+    image: "https://ik.imagekit.io/8hod93w55/14.jpg",
+  },
+] as const;
