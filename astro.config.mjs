5bfe7d35ee33fc68a9e99875a43d99662432a1f3
Gil Nimer <gilmarianimer@gmail.com>
2023-10-15 00:26:38 +0300

    disable pwa env param
    disable pwa env param

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,15 @@
 import { defineConfig } from "astro/config";
 import AstroPWA from "@vite-pwa/astro";
+import { loadEnv } from "vite";
+
+const { DISABLE_PWA } = loadEnv(import.meta.env.MODE, process.cwd(), "");
 
 // https://astro.build/config
 export default defineConfig({
   output: "static",
   integrations: [
     AstroPWA({
+      selfDestroying: DISABLE_PWA === true || DISABLE_PWA === "true",
       manifest: {
         name: "Ronnie Nimer's Portfolio",
         short_name: "Ronnie Nimer",
--- src/env.d.ts
+++ src/env.d.ts
@@ -1,6 +1,7 @@
 /// <reference types="astro/client" />
 
 interface ImportMetaEnv {
-  readonly SITE_PUBLIC_KEY: string;
-  readonly BUILDER_API_PUBLIC_KEY: string;
+  readonly SITE_PUBLIC_KEY?: string;
+  readonly BUILDER_API_PUBLIC_KEY?: string;
+  readonly DISABLE_PWA?: boolean;
 }
