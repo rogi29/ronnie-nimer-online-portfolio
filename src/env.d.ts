1eddb9ef5d01271bf7ccfd4ef946a93527640844
Gil Nimer <gilmarianimer@gmail.com>
2023-10-21 09:43:35 +0300

    remove unnecessary

--- src/env.d.ts
+++ src/env.d.ts
@@ -1,7 +1,6 @@
 /// <reference types="astro/client" />
 
 interface ImportMetaEnv {
-  readonly SITE_PUBLIC_KEY?: string;
   readonly BUILDER_API_PUBLIC_KEY?: string;
   readonly DISABLE_PWA?: boolean;
 }
